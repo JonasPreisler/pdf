bf357aaeae5457c49e4d6e9957f79ede3be5b299
Jonas Preisler <jonas.preisler@gmail.com>
2018-05-30 21:14:07 +0800

    only landing page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_one :seller
   has_one :bot
   has_many :bot_users, dependent: :destroy
 end
